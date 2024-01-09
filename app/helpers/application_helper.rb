6fe23394a70108e950f9ea7a6eb666b515272df5
Pablo Peuscovich <ppeusco@gmail.com>
2024-01-09 20:07:13 -0300

    REQ-08: Remove logic from header view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def header_content
+    return unless user_signed_in?
+
+    content_tag(:div, class: "flex items-center") do
+      concat content_tag(:span, current_user.email, class: "text-white mr-4")
+      concat button_to(t('layout.header.sign_out'), destroy_user_session_path, method: :delete, class: "text-white")
+    end
+  end
 end
