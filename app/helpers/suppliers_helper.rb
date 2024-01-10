bfd7b61ea8b71a627718b089efcbe26e2b3fc5b3
Pablo Peuscovich <ppeusco@gmail.com>
2024-01-10 12:48:09 -0300

    REQ-08: Show 'Sin información en la tabla' when the attribute is nil

--- app/helpers/suppliers_helper.rb
+++ app/helpers/suppliers_helper.rb
@@ -1,5 +1,19 @@
 module SuppliersHelper
+  ATTRIBUTE_LIST = %i[nit contact_person_phone bank_account_number].freeze
+
+  def display_info(attribute, value)
+    return value if valid_attribute?(attribute, value)
+
+    'Sin información en la tabla'
+  end
+
   def bank_name_for_supplier(supplier)
     supplier&.bank&.name
+  end
+
+  private
+
+  def valid_attribute?(attribute, value)
+    ATTRIBUTE_LIST.include?(attribute) && value.present?
   end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe ApplicationHelper, type: :helper do
     it 'devuelve la URL y el método correctos para un objeto persistido' do
       object = FactoryBot.create(:bank)
 
-      expect(helper.form_url_and_method(object)).to eq({ url: send("#{object.class.to_s.downcase.pluralize}_path", object, locale: I18n.locale), method: :patch })
+      expect(helper.form_url_and_method(object)).to eq({ url: send("#{object.class.to_s.downcase}_path", object, locale: I18n.locale), method: :patch })
     end
 
     it 'devuelve la URL y el método correctos para un objeto no persistido' do
